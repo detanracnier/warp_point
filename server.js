21ae551204239fe96a22cb656ed4f5912b40ce66
Spencer Rosio <spencerrosio3d@gmail.com>
2021-03-18 19:52:44 -0500

    last of sanity

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static("client/public"));
+
 
 // Add routes
 app.use(routes);
